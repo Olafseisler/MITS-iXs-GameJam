93fddeb6da5142dc48e43d5d8e067f60ac790106
Timo654 <41972888+Timo654@users.noreply.github.com>
2022-05-21 10:26:17 +0300

    Switch to a Dictionary for holding Words

--- Assets/Scripts/WordBank.cs
+++ Assets/Scripts/WordBank.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using System.IO;
 using System.Linq;
-
+using System.Collections.Generic;
 public class WordBank : MonoBehaviour
 {
-    public Word[] Verbs { get; set; }
-    public Word[] Nouns { get; set; }
-    public Word[] Adjectives { get; set; }
-    public Word[] Subjectives { get; set; }
-    public Word[] Conjunctions { get; set; }
+    public Dictionary<string, Word[]> wordDictionary = new()
+    {
+        {"verbs", new Word[] {} },
+        {"nouns", new Word[] {} },
+        {"adjectives", new Word[] {} },
+        {"subjectives", new Word[] {} },
+        {"conjunctions", new Word[] {} },
+    };
+
     static Word[] ReadWords(string path, WordType type)
 
     {
@@ -26,40 +30,48 @@ public class WordBank : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Verbs = ReadWords("Assets/Resources/verbs.txt", WordType.Verb);
-        Nouns = ReadWords("Assets/Resources/nouns.txt", WordType.Noun);
-        Adjectives = ReadWords("Assets/Resources/adjectives.txt", WordType.Adjective);
-        Subjectives = ReadWords("Assets/Resources/subjectives.txt", WordType.Subjective);
-        Conjunctions = ReadWords("Assets/Resources/conjunctions.txt", WordType.Conjunction);
+        wordDictionary = new Dictionary<string, Word[]>()
+        {
+            {"verbs", ReadWords("Assets/Resources/verbs.txt", WordType.Verb) },
+            {"nouns",  ReadWords("Assets/Resources/nouns.txt", WordType.Noun) },
+            {"adjectives",  ReadWords("Assets/Resources/adjectives.txt", WordType.Adjective) },
+            {"subjectives",  ReadWords("Assets/Resources/subjectives.txt", WordType.Subjective) },
+            {"conjunctions",  ReadWords("Assets/Resources/conjunctions.txt", WordType.Conjunction) },
+        };
 
-        // print all as a test
+        // print all as a test if running in editor
+        if (!Application.isEditor)
+        {
+            return;
+        }
+
         Debug.Log("--VERBS--");
-        foreach (Word word in Verbs)
-        {    
+        foreach (Word word in wordDictionary["verbs"])
+        {
             Debug.Log(word.WordText);
         }
 
         Debug.Log("--NOUNS--");
-        foreach (Word word in Nouns)
-        {    
+        foreach (Word word in wordDictionary["nouns"])
+        {
             Debug.Log(word.WordText);
         }
 
         Debug.Log("--ADJECTIVES--");
-        foreach (Word word in Adjectives)
-        {      
+        foreach (Word word in wordDictionary["adjectives"])
+        {
             Debug.Log(word.WordText);
         }
 
         Debug.Log("--SUBJECTIVES--");
-        foreach (Word word in Subjectives)
-        {     
+        foreach (Word word in wordDictionary["subjectives"])
+        {
             Debug.Log(word.WordText);
         }
 
         Debug.Log("--CONJUNCTIONS--");
-        foreach (Word word in Conjunctions)
-        {          
+        foreach (Word word in wordDictionary["conjunctions"])
+        {
             Debug.Log(word.WordText);
         }
     }
